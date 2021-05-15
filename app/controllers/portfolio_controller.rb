3f79cec78e16c6c276e23bddf97231889f510791
myerkes <mattyerkes23@gmail.com>
2021-05-15 11:09:58 -0400

    Create navigation page controllers. Update footer css

---
+++ app/controllers/portfolio_controller.rb
@@ -0,0 +1,8 @@
+class PortfolioController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#welcome'
 
+  get '/portfolio' => 'portfolio#index'
+  get '/portfolio/:id' => 'portfolio#show'
+
+  get '/about' => 'about#index'
+
+  get '/contact' => 'contact#index'
+
   resources :pages
 end
