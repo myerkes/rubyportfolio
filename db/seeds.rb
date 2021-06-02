d89ec92d6d73718b04c5f8d0cc29d3228cb3a909
myerkes <mattyerkes23@gmail.com>
2021-06-02 15:01:31 -0400

    Upload hackerrank logo and reformat footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #Project.create(title: "", description: "", picture: "", link: "")
 Project.create(title: "Misquote", description: "A Ruby on Rails Web Application that let's users learn all the famous quote they've actually been saying wrong.", picture: "https://i.imgur.com/FU6FdO4.png", link: "https://cryptic-reef-43844.herokuapp.com/")
-Project.create(title: "My Portfolio", description: "My personal portfolio is a Ruby on Rails Web Application.", picture: "https://imgur.com/f4lGwj7.png", link: "https://ancient-beach-53530.herokuapp.com/")
+Project.create(title: "My Portfolio", description: "My personal portfolio is a Ruby on Rails Web Application.", picture: "https://imgur.com/f4lGwj7.png", link: "https://matthewyerkes.herokuapp.com/")
