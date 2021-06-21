ea3cf7c0b31e0f61704c1989126a9732667e2ea8
myerkes <mattyerkes23@gmail.com>
2021-06-21 17:54:46 -0400

    Create create and delete path for projects

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,10 +1,38 @@
 class PortfolioController < ApplicationController
 
+    before_action :require_user, except: [:index, :show]
+
     def index
         @projects = Project.all
     end
 
     def show
         @project = Project.find(params[:id])
+    end
+
+    def new
+        @new_project = Project.new()
+    end
+
+    def create
+        @project = Project.new(project_params)
+
+        if @project.save
+            redirect_to '/portfolio'
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        @project = Project.find(params[:id])
+        @project.destroy
+
+        redirect_to '/portfolio'
+    end
+
+    private
+    def project_params
+        params.require(:project).permit(:title, :description, :link, :picture)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#welcome', as: :home
 
-  get '/portfolio' => 'portfolio#index', as: :portfolio
-  get '/portfolio/:id' => 'portfolio#show', as: :project
-
   get '/about' => 'about#index', as: :about
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 
+  post '/portfolio/new' => 'portfolio#create'
+
+  resources :portfolio
   resources :pages
   resources :contacts
   resources :users
-
 end
