288f3250245cf9ca68e951be1e22c6e791b462cc
Matthew Yerkes <mattyerkes23@gmail.com>
2021-07-22 09:44:11 -0400

    Add Django Twitter to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #Project.create(title: "", description: "", picture: "", link: "")
+Project.create(title: "Django Twitter", description: "A Django Web App that's a functional copy of Twitter. Users can create accounts (or log in through Google), post tweets, comment, retweet, etc.", picture: "https://imgur.com/lQfEV8Q.png", link: "https://lit-brushlands-13013.herokuapp.com/")
 Project.create(title: "Misquote", description: "A Ruby on Rails Web Application that let's users learn all the famous quote they've actually been saying wrong.", picture: "https://i.imgur.com/FU6FdO4.png", link: "https://cryptic-reef-43844.herokuapp.com/")
-Project.create(title: "My Portfolio", description: "My personal portfolio is a Ruby on Rails Web Application.", picture: "https://imgur.com/f4lGwj7.png", link: "https://matthewyerkes.herokuapp.com/")
+Project.create(title: "My Portfolio", description: "My personal portfolio is a Ruby on Rails Web Application.", picture: "https://imgur.com/6IDeOkZ.png", link: "https://matthewyerkes.herokuapp.com/")
 Project.create(title: "Django Delights", description: "A Django Web App that's an example of a restaurant website. The restaurant staff are able to log in an edit things like the menu or their ingredient inventory.", picture: "https://imgur.com/uzy19lZ.png", link: "https://myerkes.pythonanywhere.com/")
